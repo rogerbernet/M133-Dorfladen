59f8db77f662e3806c696cf5278b3f9f8bcae1ba
rogerbernet <75241844+rogerbernet@users.noreply.github.com>
2022-01-06 11:11:50 +0100

    beginn

--- Webserver.js
+++ Webserver.js
@@ -1,1 +1,10 @@
 "use strict"
+import {Application} from "./deps.js";
+import {apiRoutes} from "./backend/fileserver.js";
+import {frontendFiles} from ".backend/fileserver.js"
+
+const app = new Application();
+
+app.use(apiRoutes);
+app.use(frontendFiles);
+app.listen({porst: 8000});
